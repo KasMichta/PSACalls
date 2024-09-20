ce33bdf032bbae2efacd8fca31924c3515981f6a
KasMichta <kacper.michta33@gmail.com>
2024-09-20 12:01:55 +0100

    First implementation of Invoke-PSARequest with tests for request params

--- PSACalls/Public/Invoke-PSARequest.Tests.ps1
+++ PSACalls/Public/Invoke-PSARequest.Tests.ps1
@@ -5,9 +5,51 @@ BeforeAll {
 }
 
 Describe "'Invoke-PSARequest' Function Tests" {
-    Context 'Ran Once' {
-        It 'Should run' {
-            Invoke-PSARequest | Should -Be "Invoke-PSARequest Ran. Method: GET"
+    Context 'API Call Methods' {
+        BeforeEach{
+            Mock Invoke-WebRequest {
+                param($method, $uri, $headers, $body)
+                Write-Output "Method: $method"
+            }
+        }
+        It 'Should return <_> Method' -ForEach 'GET', 'POST', 'PATCH', 'DELETE' {
+            Invoke-PSARequest -method $_ -uri 'https://api.example.com' | Should -Be "Method: $_"
+        }
+    }
+
+    Context 'API Call URI' {
+        BeforeEach{
+            Mock Invoke-WebRequest {
+                param($method, $uri, $headers, $body)
+                Write-Output "URI: $uri"
+            }
+        }
+        It 'Should return URI' {
+            Invoke-PSARequest -uri 'https://api.example.com/' | Should -Be 'URI: https://api.example.com/'
+        }
+    }
+
+    Context 'API Call Headers' {
+        BeforeEach{
+            Mock Invoke-WebRequest {
+                param($method, $uri, $headers, $body)
+                Write-Output "Headers: $headers"
+            }
+        }
+        It 'Should return Headers' {
+            Invoke-PSARequest -uri 'https://api.example.com' -headers @{ 'Content-Type' = 'application/json' } | Should -Be 'Headers: System.Collections.Hashtable'
+        }
+    }
+
+    Context 'API Call Body' {
+        BeforeEach{
+            Mock Invoke-WebRequest {
+                param($method, $uri, $headers, $body)
+                Write-Output "Body: $body"
+            }
+        }
+        It 'Should return Body' {
+            Invoke-PSARequest -uri 'https://api.example.com' -body 'Test Body' | Should -Be 'Body: Test Body'
         }
     }
 }
--- PSACalls/Public/Invoke-PSARequest.ps1
+++ PSACalls/Public/Invoke-PSARequest.ps1
@@ -10,14 +10,40 @@ Function Invoke-PSARequest {
     )
 
     begin {
-        Write-Verbose "Running Invoke-PSARequest"
+        If ($null -eq $env:PSA_APIKey -or $null -eq $env:PSA_ClientID) {
+            Throw "PSA_APIKey and PSA_ClientID must be set in the environment variables."
+        }
+        Write-Verbose "Method: $method"
+        Write-Verbose "URI: $uri"
+        If ($Body) {
+            Write-Verbose "Body Included in Request (Length: $($Body.Length))"
+        }
     }
 
     process {
-        Write-Output "Invoke-PSARequest Ran. Method: $method"
+        $requestParams = @{
+            uri        = $uri
+            method     = $method
+            ContentType = 'application/json'
+            headers    = if ($headers) {
+                $headers
+            } else {
+                @{
+                    authorization  = "basic $env:PSA_APIKey"
+                    clientid       = "$env:PSA_ClientID"
+                }
+            }
+        }
+
+        if ($body) {
+            $requestParams.Add('body', $body)
+        }
+
+        $response = Invoke-WebRequest @requestParams -ErrorAction Stop
     }
 
     end {
+        Write-Output $response
         Write-Verbose "Invoke-PSARequest Complete."
     }
 }
